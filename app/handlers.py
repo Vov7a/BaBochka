dd6ddbc8830cf7d14f7c27e2bbbd6072bc498931
Vov7a <106620836+Vov7a@users.noreply.github.com>
2024-09-15 17:22:52 +0300

    version 1.2.2

--- app/handlers.py
+++ app/handlers.py
@@ -1,5 +1,6 @@
 import asyncio
 import app.keyboards as kb
+import datetime
 
 from aiogram import Router
 from aiogram.filters import CommandStart, Command
@@ -10,7 +11,17 @@ router = Router()
 
 @router.message(CommandStart())
 async def handle_start(message: Message):
-    new_msg = await message.answer('Доброе утро!')
+    now = datetime.datetime.now().hour
+    
+    if now >= 6 and now < 12:
+        new_msg = await message.answer('Доброе утро!', reply_markup=kb.start)
+    elif now >= 12 and now < 17:
+        new_msg = await message.answer('Добрый день!', reply_markup=kb.start)
+    elif now >= 17 and now < 21:
+        new_msg = await message.answer('Добрый вечер!', reply_markup=kb.start)
+    else:
+        new_msg = await message.answer('Доброй ночи!', reply_markup=kb.start)
+        
     await message.delete()
     await asyncio.sleep(20)
     try:
